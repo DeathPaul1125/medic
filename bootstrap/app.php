c3d11be504fe0e37dc6c77a0d1785d0231027441
Pablo <civernet@gmail.com>
2025-09-28 20:32:13 -0600

    Cambiando el home desde fotify

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
         //Agregando mis propias rutas
         then: function (){
-            Route::middleware('web')
+            Route::middleware('web', 'auth')
                 ->prefix('admin')
                 ->name('admin.')
                 ->group(base_path('routes/admin.php'));
